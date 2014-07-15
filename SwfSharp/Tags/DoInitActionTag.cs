f48f0278ea4ca109a49628edacd5f6d288381ab0
Alexander Vostres <alex.vostres@gmail.com>
2014-07-15 14:00:38 +0300

    implement writing of DoInitActionTag

--- SwfSharp/Tags/DoInitActionTag.cs
+++ SwfSharp/Tags/DoInitActionTag.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using SwfSharp.Structs;
 using SwfSharp.Utils;
@@ -18,6 +19,12 @@ namespace SwfSharp.Tags
         {
             SpriteID = reader.ReadUI16();
             base.FromStream(reader, swfVersion);
+        }
+
+        internal override void ToStream(BitWriter writer, byte swfVersion)
+        {
+            writer.WriteUI16(SpriteID);
+            base.ToStream(writer, swfVersion);
         }
     }
 }
